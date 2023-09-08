6d631d6eb3c012c2035c04515338c4bfe2d8af27
wakeupingear <abbysfarhat@gmail.com>
2023-09-08 00:24:11 -0700

    fix: linting + ci condition

--- editors/scene/src/components/panels/ActorPanel.tsx
+++ editors/scene/src/components/panels/ActorPanel.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useEditor } from '../EditorWrapper';
 
 export default function ActorPanel() {
--- editors/scene/src/components/panels/ScenePanel.tsx
+++ editors/scene/src/components/panels/ScenePanel.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useEditor } from '../EditorWrapper';
 
 export default function ScenePanel() {
